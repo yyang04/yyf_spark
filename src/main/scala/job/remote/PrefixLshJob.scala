92cd94e863c224f700ba474a508d819e35ddbc3a
yangyufeng04 <yangyufeng04@meituan.com>
2022-07-20 11:39:26 +0800

    67314155 - test

--- src/main/scala/job/remote/PrefixLshJob.scala
+++ src/main/scala/job/remote/PrefixLshJob.scala
@@ -7,6 +7,7 @@ import utils.privacy_clustering.PrefixLshClustering
 
 object PrefixLshJob extends RemoteSparkJob {
     override def run(): Unit = {
+        val threshold = params.threshold
         val data = spark.sql(
             """select uuid, user_emb
               |from mart_waimaiad.privacy_clustering_user_emb_test
@@ -16,28 +17,37 @@ object PrefixLshJob extends RemoteSparkJob {
             val user_emb = row.getAs[Seq[Double]](1).toArray
             (uuid, user_emb)
         })
+
         val model = new PrefixLshClustering(20, 1, 2000)
         val result = model.fit(data)
-        saveAsTable(result, "mart_waimaiad.privacy_clustering_user_cluster_test_prefixlsh", "20211125")
+
+        saveAsTable(rdd=result,
+            tableName="mart_waimaiad.privacy_clustering_user_cluster_test",
+            date="20211125",
+            threshold=threshold,
+            algorithm="prefixlsh")
     }
 
-    def saveAsTable(result: RDD[(String, Array[Double], Array[Double])],
+    def saveAsTable(rdd: RDD[(String, Array[Double], Array[Double])],
                     tableName: String,
-                    date: String): DataFrame = {
+                    date: String,
+                    threshold: Int,
+                    algorithm: String): DataFrame = {
+
         spark.sql(s"""
                 create table if not exists $tableName (
                     uuid string,
                     user_emb array<double>,
                     cluster_center array<double>
-                ) partitioned by (dt string)
+                ) partitioned by (dt string, threshold int, algorithm string)
                 STORED AS ORC
             """.stripMargin)
 
         val temp_input_data = "temp_input_data"
-        val df = result.toDF("uuid", "user_emb", "cluster_center")
+        val df = rdd.toDF("uuid", "user_emb", "cluster_center")
         df.createOrReplaceTempView(temp_input_data)
         spark.sql(s"""
-                insert overwrite table $tableName partition (dt=$date)
+                insert overwrite table $tableName partition (dt=$date, threshold=$threshold, algorithm=$algorithm)
                 select * from (
                     $temp_input_data
             )""".stripMargin)
