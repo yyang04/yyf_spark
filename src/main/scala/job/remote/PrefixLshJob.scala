008bca8d1ecf44e57c3d0c675514887257a03b11
yangyufeng04 <yangyufeng04@meituan.com>
2022-07-20 12:03:34 +0800

    67314155 - test

--- src/main/scala/job/remote/PrefixLshJob.scala
+++ src/main/scala/job/remote/PrefixLshJob.scala
@@ -47,7 +47,7 @@ object PrefixLshJob extends RemoteSparkJob {
         val df = rdd.toDF("uuid", "user_emb", "cluster_center")
         df.createOrReplaceTempView(temp_input_data)
         spark.sql(s"""
-                insert overwrite table $tableName partition (dt=$date, threshold=$threshold, algorithm=$algorithm)
+                insert overwrite table $tableName partition (dt=$date, threshold=$threshold, algorithm='$algorithm')
                 select * from (
                     $temp_input_data
             )""".stripMargin)
