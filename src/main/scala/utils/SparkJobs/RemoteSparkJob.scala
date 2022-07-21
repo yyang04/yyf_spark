9dbe9ef6dfbf246519cb37173d4af992d766f9c4
yangyufeng04 <yangyufeng04@meituan.com>
2022-07-22 02:40:51 +0800

    67314155 - test

--- src/main/scala/utils/SparkJobs/RemoteSparkJob.scala
+++ src/main/scala/utils/SparkJobs/RemoteSparkJob.scala
@@ -37,6 +37,8 @@ abstract class RemoteSparkJob extends ArgsParser with SQLImplicits with Serializ
               .set("spark.yarn.executor.memoryOverhead","1024")
               .set("spark.yarn.driver.memoryOverhead","1024")
               .set("spark.hadoop.hive.mt.renew.token.enable", "true")
+              .set("spark.hadoop.hive.mt.renew.token.enable", "true")
+              .set("spark.driver.maxResultSize","12G")
         }
         this.spark = SparkSession
           .builder()
