a97caa9998e6f631c371163ee79f5b13c31b1508
yangyufeng04 <yangyufeng04@meituan.com>
2022-10-26 12:09:20 +0800

    67314155 test

--- src/main/scala/job/remote/flashbuy/Cid2Item.scala
+++ src/main/scala/job/remote/flashbuy/Cid2Item.scala
@@ -13,12 +13,9 @@ object Cid2Item extends RemoteSparkJob {
             s"""
                |select concat_ws('_', cid3, poi_geohash) as cate3Id_geohash,
                |       sku_id,
-               |       cast(  case when event_type='click' then 1.0 * cnt
-               |                   when event_type='order' then 0.5 * cnt
-               |                   when event_type='cart'  then 0.5 * cnt
-               |                   else 0.0 end as weight_cnt) as double )
+               |       cnt
                |from (
-               |      select a.sku_id, poi_geohash, b.cid3, event_type, count(*) as cnt
+               |      select a.sku_id, poi_geohash, b.cid3, count(*) as cnt
                |        from mart_waimaiad.recsys_linshou_user_explicit_acts a
                |        join ( select product_id as sku_id,
                |                      third_category_id as cid3
@@ -29,16 +26,16 @@ object Cid2Item extends RemoteSparkJob {
                |        and poi_geohash is not null
                |        and a.sku_id is not null
                |        and event_type in ('click','order','cart')
-               |      group by 1,2,3,4)
-               |where cnt >= 2
+               |      group by 1,2,3)
+               |where cnt >= 3
                |""".stripMargin).rdd.map(row => {
             val cate3Id_geohash = row.getAs[String](0)
             val sku_id = row.getAs[Long](1)
-            val cnt = row.getAs[Double](2)
+            val cnt = row.getAs[Long](2)
             (cate3Id_geohash, (sku_id, cnt))
         }).groupByKey.mapValues(iter => {
             val entities = iter.toArray.sortBy(_._2).takeRight(100)
-            val factors = ArrayOperations.softmax(entities.map(_._2))
+            val factors = ArrayOperations.softmax(entities.map(_._2.toDouble))
             val results = entities.map(_._1).zip(factors).map(x=>s"${x._1}:${x._2}")
             results
         }).toDF("key", "value")
