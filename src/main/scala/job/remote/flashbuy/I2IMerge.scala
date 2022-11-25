8c3f1960ccaa288df2be77d6bd13f49caa3ff446
yangyufeng04 <yangyufeng04@meituan.com>
2022-11-25 16:01:51 +0800

    67314155 test

--- src/main/scala/job/remote/flashbuy/I2IMerge.scala
+++ src/main/scala/job/remote/flashbuy/I2IMerge.scala
@@ -20,7 +20,7 @@ object I2IMerge extends RemoteSparkJob {
             val key = row.getString(1)
             val value = row.getAs[Seq[String]](2).map{ x =>
                 val arr = x.split(":")
-                (arr(0).toLong, arr(1).toFloat)
+                (arr(0).toLong, arr(1).toFloat-0.00001)
             }
             (key, (method, value))
         }.groupByKey.map { case (key, iter) =>
