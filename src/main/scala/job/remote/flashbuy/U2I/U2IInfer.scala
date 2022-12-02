8febb93ab4eaf2f2de415ccf4a53ef0390818098
yangyufeng04 <yangyufeng04@meituan.com>
2022-12-02 18:59:48 +0800

    67314155 test

--- src/main/scala/job/remote/flashbuy/U2I/U2IInfer.scala
+++ src/main/scala/job/remote/flashbuy/U2I/U2IInfer.scala
@@ -1,98 +1,87 @@
 package job.remote.flashbuy.U2I
 
-import org.apache.spark.SparkFiles
-import org.apache.spark.broadcast.Broadcast
+import com.github.jelmerk.knn.scalalike.{Item, floatInnerProduct}
+import com.github.jelmerk.knn.scalalike.hnsw.HnswIndex
+import org.apache.spark.{SparkContext, SparkFiles}
 import org.apache.spark.rdd.RDD
-import org.tensorflow.SavedModelBundle
-import utils.FileOperations.saveAsTable
-import utils.Murmurhash.hashString
 import utils.SparkJobs.RemoteSparkJob
-import utils.TimeOperations.getDateDelta
+import utils.{ArrayOperations, FileOperations}
+import utils.Murmurhash.hashString
 
-object U2IInfer extends RemoteSparkJob {
 
-//    override def run(): Unit = {
-//        // get params
-//        val mode = params.mode
-//        val dt = params.dt
-//        val model_path = params.model_path
-//
-//
-//        sc.addFile(model_path, true)
-//        val path = SparkFiles.get("tfModel")
-//        // val bundle = tf.SavedModelBundle.load(path, "serve")
-//        val broads = sc.broadcast(bundle)
-//
-//        if (mode.split(",") contains "uuid") {
-//            val df = fetch_user_embedding(dt, broads).toDF("key", "embedding")
-//            saveAsTable(spark, df, "pt_multirecall_u2i_embedding", Map("dt" -> dt, "part" -> "uuid"))
-//        }
-//
-//        if (mode.split(",") contains "sku") {
-//            val df = fetch_sku_embedding(dt, broads).toDF("key", "embedding")
-//            saveAsTable(spark, df, "pt_multirecall_u2i_embedding", Map("dt" -> dt, "part" -> "sku_id"))
-//        }
-//        bundle.close()
-//    }
-//
-//
-//    def fetch_user_embedding(dt: String, broads: Broadcast[SavedModelBundle]): RDD[(String, Array[Float])] = {
-//
-//        val uuid_embedding = spark.sql(
-//            s"""
-//               |select distinct uuid
-//               |from mart_waimaiad.lingshou_pt_ka_behavior_details_v1
-//               |where dt between ${getDateDelta(dt, -14)} and $dt
-//               |  and city_id in (110100)
-//               |""".stripMargin).rdd.map { row =>
-//            val uuid = row.getAs[String](0)
-//            (uuid, hashString("uuid_", uuid))
-//        }.mapPartitions { iter =>
-//            val arr = iter.toArray
-//            val tmp = arr.map(x => Array(x._2, 0L))
-//            val sess = broads.value.session()
-//            val x = tf.Tensor.create(tmp)
-//            val y = sess.runner()
-//              .feed("cat_feature:0", x)
-//              .fetch("user_embedding:0").run().get(0)
-//
-//            val result = Array.ofDim[Float](y.shape()(0).toInt, y.shape()(1).toInt)
-//            y.copyTo(result)
-//            val res = arr.map(_._1).zip(result)
-//            res.iterator
-//        }
-//        uuid_embedding
-//    }
-//
-//    def fetch_sku_embedding(dt: String, broads: Broadcast[SavedModelBundle]): RDD[(String, Array[Float])] = {
-//
-//        val sku_embedding = spark.sql(
-//            s"""
-//               |select sku_id, poi_id, first_category_id, second_category_id, third_category_id
-//               |  from mart_waimaiad.recsys_linshou_pt_poi_skus
-//               | where dt=$dt
-//               |""".stripMargin).rdd.map { row =>
-//            val sku_id = row.getAs[Long](0)
-//            val poi_id = row.getAs[Long](1)
-//            val first_category_id = row.getAs[Long](2)
-//            val second_category_id = row.getAs[Long](3)
-//            val third_category_id = row.getAs[Long](4)
-//
-//            (poi_id, (sku_id, first_category_id, second_category_id, third_category_id))
-//            (sku_id, hashString("sku_id_", sku_id.toString))
-//        }.mapPartitions { iter =>
-//            val arr = iter.toArray
-//            val tmp = arr.map(x => Array(0L, x._2))
-//            val sess = broads.value.session()
-//            val x = tf.Tensor.create(tmp)
-//            val y = sess.runner()
-//              .feed("cat_feature:0", x)
-//              .fetch("sku_embedding:0").run().get(0)
-//            val result = Array.ofDim[Float](y.shape()(0).toInt, y.shape()(1).toInt)
-//            y.copyTo(result)
-//            val res = arr.map(_._1).zip(result)
-//            res.iterator
-//        }.toDF("sku_id", "poi_id", "feature_vector")
-//        sku_embedding
-//    }
+case class SkuInfo(id: String, vector: Array[Float]) extends Item[String, Array[Float]] {
+    override def dimensions: Int = vector.length
 }
+case class UserInfo(id: String, vector: Array[Float]) extends Item[String, Array[Float]] {
+    override def dimensions: Int = vector.length
+}
+
+object SkuEmbGen extends RemoteSparkJob {
+    override def run(): Unit = {
+        val dt = params.dt
+
+        val threshold = params.threshold
+        val threshold2 = params.threshold2
+
+        val user_path = "viewfs://hadoop-meituan/user/hadoop-hmart-waimaiad/yangyufeng04/bigmodel/multirecall/20221202_115147/user_embedding/"
+        val sku_path = "viewfs://hadoop-meituan/user/hadoop-hmart-waimaiad/yangyufeng04/bigmodel/multirecall/20221202_115147/sku_embedding/"
+        // params
+        val user = read_raw(sc, user_path)
+        val sku = read_raw(sc, sku_path)
+
+        val poi_sku = spark.sql(
+            s"""
+               |select cast(sku_id as string) as sku_id,
+               |       poi_id
+               |  from mart_waimaiad.recsys_linshou_pt_poi_skus
+               | where dt=$dt and city_id=110100
+               |from mart_waimaiad.recsys_linshou_pt_poi_skus
+               |""".stripMargin).rdd.map { row =>
+            val sku_id = row.getAs[String](0)
+            val poi_id = row.getAs[Long](0)
+            (sku_id, poi_id)
+        }.join(sku).map { case (sku, (poi, emb)) => (poi, SkuInfo(sku, emb)) }.groupByKey
+
+        val poi_user = spark.sql(
+            s"""
+               |select uuid, pois
+               |  from mart_waimaiad.recsys_linshou_user_vecs
+               | where dt=$dt and city_id=110100
+               |""".stripMargin).rdd.map { row =>
+            val uuid = row.getAs[String](0)
+            val pois = row.getAs[Seq[Long]](1)
+            (uuid, pois)
+        }.join(user).flatMap { case (uuid, (pois, emb)) => pois.map(poi => (poi, UserInfo(uuid, emb))) }.groupByKey
+
+        val res = poi_sku.join(poi_user).flatMap {
+            case (poi, (skus, users)) =>
+                val index = HnswIndex[String, Array[Float], SkuInfo, Float](32, floatInnerProduct, maxItemCount = skus.size, m = 48, ef = 200, efConstruction = 200)
+                index.addAll(skus)
+                users.par.map { user =>
+                    val vector = index.findNearest(user.vector, threshold)
+                      .map(re => (re.item().id, (1 - re.distance()).toDouble)).toArray
+                    (user.id, (poi, vector, vector.map(_._2).sum / vector.length))
+                }.toArray
+        }.groupByKey.mapValues { iter =>
+            val result = iter.toArray.sortBy(_._3).reverse.take(threshold2)
+            val tmp = result.flatMap {
+                case (poi_id, skuIdList, average) => skuIdList
+            }
+            val scores = ArrayOperations.maxScale(tmp.map(_._2))
+            tmp.map(_._1).zip(scores).map { case (sku_id, score) => s"$sku_id:${"%.5f".format(score)}" }
+        }.toDF("key", "value")
+
+
+        val partition = Map("date" -> dt, "branch" -> "u2i", "method" -> "dual_tower_v1")
+        FileOperations.saveAsTable(spark, res, "recsys_linshou_multi_recall_results_v2", partition)
+    }
+
+    def read_raw(sc: SparkContext, path: String): RDD[(String, Array[Float])] = {
+        sc.textFile(path).map { row =>
+            val id = row.split(",")(0)
+            val emb = row.split(",").drop(1).map(_.toFloat)
+            (id, emb)
+        }
+    }
+}
+
