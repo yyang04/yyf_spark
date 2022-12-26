cfb2ea9b8b511717e89e5b85c7bbfb2ff9a06e72
yangyufeng04 <yangyufeng04@meituan.com>
2022-12-26 16:56:55 +0800

    67314155 test

--- src/main/scala/job/remote/flashbuy/u2i/SeqFeature.scala
+++ src/main/scala/job/remote/flashbuy/u2i/SeqFeature.scala
@@ -5,15 +5,18 @@ import utils.FileOperations.saveAsTable
 object SeqFeature extends RemoteSparkJob {
     override def run(): Unit = {
         val dt = params.dt
-        val src_table = params.src_table_name
-        val dst_table = params.dst_table_name
+        action(src_table="mart_waimaiad.lingshou_user_click", dst_table="lingshou_user_click_tag_sanitized_v2", dt=dt)
+        action(src_table="mart_waimaiad.lingshou_user_cart", dst_table="lingshou_user_cart_tag_sanitized_v2", dt=dt)
+        action(src_table="mart_waimaiad.lingshou_user_order", dst_table="lingshou_user_order_tag_sanitized_v2", dt=dt)
+    }
 
+    def action(src_table:String, dst_table:String, dt:String):Unit = {
         val init = spark.sql(
             s"""
-              |select uuid, sku_list
-              |  from $src_table
-              | where dt=$dt
-              |""".stripMargin).rdd.flatMap{ row =>
+               |select uuid, sku_list
+               |  from $src_table
+               | where dt=$dt
+               |""".stripMargin).rdd.flatMap { row =>
             val uuid = row.getString(0)
             val sku_list = row.getSeq[String](1)
             val sku_timestamp = sku_list.flatMap { sku_timestamp =>
@@ -24,8 +27,8 @@ object SeqFeature extends RemoteSparkJob {
                     case _: Throwable => None
                 }
             }
-            sku_timestamp.map{
-                case(sku_id, timestamp) => (sku_id, (uuid, timestamp))
+            sku_timestamp.map {
+                case (sku_id, timestamp) => (sku_id, (uuid, timestamp))
             }
         }
 
@@ -40,19 +43,21 @@ object SeqFeature extends RemoteSparkJob {
                |   AND is_valid = 1
                |   AND is_online_poi_flag = 1
                |   AND tag_id is not null AND tag_id != ''
-               |   """.stripMargin).rdd.map{ row =>
+               |   """.stripMargin).rdd.map { row =>
             val sku_id = row.getLong(0)
             val tag_id = row.getString(1).toLong
             (sku_id, tag_id)
-        }.join(init).map{ case (sku_id, (tag_id, (uuid, timestamp))) => (uuid, (tag_id, timestamp))
-        }.groupByKey.mapValues{ iter =>
+        }.join(init).map { case (sku_id, (tag_id, (uuid, timestamp))) => (uuid, (tag_id, timestamp))
+        }.groupByKey.mapValues { iter =>
             val tag_list = iter.toList.sortBy(_._2).reverse.map(_._1)
             val tag_map = tag_list.groupBy(identity).mapValues(_.size)
             val tag_seq_list = tag_list.distinct
             val tag_freq_list = tag_seq_list.map(x => tag_map(x))
             (tag_seq_list, tag_freq_list)
-        }.map{ case(uuid, (tag_list, tag_freq_list)) => (uuid, tag_list, tag_freq_list)}.toDF("uuid", "tag_list", "tag_freq_list")
-        saveAsTable(spark, sku_info, dst_table, Map("dt" -> dt) )
+        }.map { case (uuid, (tag_list, tag_freq_list)) => (uuid, tag_list, tag_freq_list) }.toDF("uuid", "tag_list", "tag_freq_list")
+        saveAsTable(spark, sku_info, dst_table, Map("dt" -> dt))
+
+
     }
 
 
