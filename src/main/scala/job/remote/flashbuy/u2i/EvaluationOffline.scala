424c9fa73d75c4b99aed224b1aa7fc5cd5b4a2e9
yangyufeng04 <yangyufeng04@meituan.com>
2023-01-04 16:06:53 +0800

    67314155 test

--- src/main/scala/job/remote/flashbuy/u2i/EvaluationOffline.scala
+++ src/main/scala/job/remote/flashbuy/u2i/EvaluationOffline.scala
@@ -7,17 +7,10 @@ import scala.util.control.Breaks
 
 object EvaluationOffline {
     def main(args: Array[String]): Unit = {
-        val loop = new Breaks
-        loop.breakable {
-            for (i <- 1 to 10) {
-                for (j <- 1 to 10) {
-                    println(i)
-                    if (i == 2) {
-                        loop.break()
-                    }
-                }
-            }
-        }
+
+
+
+
 
     }
 }
