c85f591fc650f1395e49cbbb2589c72f82228243
yangyufeng04 <yangyufeng04@meituan.com>
2023-02-13 17:47:46 +0800

    67314155 test

---
+++ src/main/scala/job/remote/flashbuy/u2i/Feature/SeqEntity.scala
@@ -0,0 +1,8 @@
+package job.remote.flashbuy.u2i.Feature
+
+case class SeqEntity(sku_id: Long,
+               first_category_id: Long,
+               second_category_id: Long,
+               third_category_id: Long,
+               tag_id: Long
+              )
