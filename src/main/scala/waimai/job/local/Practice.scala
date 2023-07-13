c643db8a0565628d6652a487d66b608c6c049565
yangyufeng04 <yangyufeng04@meituan.com>
2023-07-13 11:14:39 +0800

    67314155 test

--- src/main/scala/waimai/job/local/Practice.scala
+++ src/main/scala/waimai/job/local/Practice.scala
@@ -1,26 +1,17 @@
 package waimai.job.local
 
 import waimai.utils.SparkJobs.LocalSparkJob
+import scala.concurrent.duration._
 
-object Practice extends LocalSparkJob{
-    override def run(): Unit = {
-
-        val data = Seq(1,5,7,10,23,20,6,5,10,7,10)
+object Practice {
 
 
+    def main(args: Array[String]): Unit = {
 
-
-
-
-
-
-
-
-
-
-
-
-
+        (1 to 10).foreach { x =>
+            Thread.sleep(30.seconds.toMillis)
+            println(x)
+        }
 
     }
 }
