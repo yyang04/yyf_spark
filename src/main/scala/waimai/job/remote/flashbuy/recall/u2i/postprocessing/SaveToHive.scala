6a862df365914a8c6df9fc600e1a2e9570d2da80
yangyufeng04 <yangyufeng04@meituan.com>
2023-08-09 17:43:47 +0800

    67314155 test

--- src/main/scala/waimai/job/remote/flashbuy/recall/u2i/postprocessing/SaveToHive.scala
+++ src/main/scala/waimai/job/remote/flashbuy/recall/u2i/postprocessing/SaveToHive.scala
@@ -3,6 +3,7 @@ package waimai.job.remote.flashbuy.recall.u2i.postprocessing
 import org.apache.spark.rdd.RDD
 import waimai.utils.SparkJobs.RemoteSparkJob
 import org.apache.spark.SparkContext
+import waimai.utils.FileOp.saveAsTable
 
 object SaveToHive extends RemoteSparkJob {
     override def run(): Unit = {
@@ -10,24 +11,20 @@ object SaveToHive extends RemoteSparkJob {
         val version = params.version
         val userEmbeddingPath = params.src_table_name
         val itemEmbeddingPath = params.dst_table_name
+        val userEmbedding = readEmbedding(sc, userEmbeddingPath)
+        val itemEmbedding = readEmbedding(sc, itemEmbeddingPath)
 
-        val partition = Map("dt" -> dt, "version" -> "")
+        val userDF = userEmbedding.toDF("key", "value")
+        val userPartition = Map("dt" -> dt, "version" -> version, "entity" -> "user")
+        val itemDF = itemEmbedding.toDF("key", "value")
+        val itemPartition = Map("dt" -> dt, "version" -> version, "entity" -> "item")
+
+        saveAsTable(spark, userDF, "pt_multi_recall_results_vector", partition=userPartition)
+        saveAsTable(spark, itemDF, "pt_multi_recall_results_vector", partition=itemPartition)
     }
 
-    def readUserEmbedding(sc: SparkContext, path: String): RDD[(String, String, Array[Float])] = {
-        // request_id,uuid,embedding1,embedding2...
-        val result = sc.textFile(path).map { row =>
-            val splitPart = row.split(",")
-            val request_id = splitPart(0)
-            val uuid = splitPart(1)
-            val emb = splitPart.drop(2).map(_.toFloat)
-            (request_id, uuid, emb)
-        }
-        println(result.take(1).head)
-        result
-    }
-
-    def readItemEmbedding(sc: SparkContext, path: String): RDD[(String, Array[Float])] = {
+    def readEmbedding(sc: SparkContext, path: String): RDD[(String, Array[Float])] = {
+        // request_id_uuid,embedding1,embedding2...
         // poi_id,embedding1,embedding2
         val result = sc.textFile(path).map { row =>
             val splitPart = row.split(",")
